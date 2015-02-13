54f76c11244b0c2c4ffb43cafc38ba89b775e444
mjm07r <mcgee2515@gmail.com>
2015-02-13 13:04:39 -0500

    Added external call to maps so that directions can be found

--- app/src/main/java/com/example/root/mapstest/LocationAlert.java
+++ app/src/main/java/com/example/root/mapstest/LocationAlert.java
@@ -5,10 +5,15 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
 
 /**
  * Created by Mike McGee on 2/3/15.
@@ -37,11 +42,26 @@ public class LocationAlert extends DialogFragment {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
-                Intent intent = new Intent(getActivity(), MainActivity.class);
+            /*    Intent intent = new Intent(getActivity(), MainActivity.class);
                 Bundle bundle = new Bundle();
                 bundle.putParcelable("Coordinates", passedLocation);
                 intent.putExtra("bundle", bundle);
                 startActivity(intent);
+
+             */
+                Uri location = Uri.parse("geo:"+passedLocation.latitude+","+passedLocation.longitude+"?q="+
+                                            passedLocation.latitude+","+passedLocation.longitude);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+                //Check to make sure that an app can handle this request.
+                PackageManager packageManager = getActivity().getPackageManager();
+                List<ResolveInfo> activities = packageManager.queryIntentActivities(mapIntent, 0);
+                boolean isIntentSafe = activities.size() > 0;
+
+                //if there are activities to handle intent then start activity.
+                if(isIntentSafe){
+                    startActivity(mapIntent);
+                }
             }
         });
 
